9c959a6b6272ad78f39c331ed9289159143e0433
Shohjahon969 <shohjahonortiqboyev81@gmail.com>
2026-09-24 17:48:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import os
+import threading
+from flask import Flask
+
+app = Flask("")
+
+
+@app.route("/")
+def home():
+  return "Bot ishlamoqda!"
+
+
+def run():
+  port = int(os.environ.get("PORT", 8080))
+  app.run(host="0.0.0.0", port=port)
+
+
+threading.Thread(target=run).start()
 
